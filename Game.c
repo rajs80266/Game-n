01e63f6112cb418c4cf2957e08717378cde82dc0
Raj <raj31@somaiya.edu>
2018-06-12 12:47:12 +0530

    Added Levels and comments to code

--- Game.c
+++ Game.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    int start,n,temp,user,comp,gap;
+    int level,start,n,temp,user,comp,gap;
 
-    printf("Start by you(1)\n");
-    printf("Start by computer(2)\n");
+    //Initializing The game accoring to User
+    printf("1-Easy\n2-Medium\n3-Hard\nEnter Level:");
+    scanf("%d",&level);
+
+    printf("\n\nStart by you(1)\nStart by computer(2)\nEnter your option:");
     scanf("%d",&start);
-    system("cls");
 
-    printf("Enter number n:");
+    printf("\n\nEnter number n:");
     scanf("%d",&n);
     printf("Enter maximum number that can be increased:");
     scanf("%d",&gap);
-    gap++;
     system("cls");
+    gap++;
 
-    temp=n;
-    temp--;
+    //temp leading to win
+    temp=n-1;
     while(temp>0)
         temp=temp-gap;
     temp=temp+gap;
 
     if(start==2)
     {
-        printf("1\n");
-        comp=1;
+        if(temp<gap && temp<n && level==3)
+            comp=temp;
+        else
+            comp=1;
+        printf("%d\n",comp);
     }
     else
         comp=0;
@@ -35,9 +41,9 @@ int main()
         printf("Congratulations....\nYou Won.");
     else
     {
-        printf("Enter a number:");
         while(1)
         {
+            printf("Enter a number:");
             scanf("%d",&user);
             if(user<=n && user<comp+gap && user>comp)
             {
@@ -47,14 +53,31 @@ int main()
                     break;
                 }
 
-                while(temp<user)
-                    temp=temp+gap;
+                //depending on level leading to a game
+                if(level==3 ||( level==2 && user>(n-(gap*2)) ) || user>(n-gap) )
+                {
+                    while(temp<user)
+                        temp=temp+gap;
 
-                comp=temp;
-                if(temp==user)
-                    comp++;
+                    comp=temp;
+                    if(temp==user)
+                        comp++;
+                }
+                //if level is low let computer play randomly
+                else
+                {
+                    srand(time(0));
+                    comp = ( rand()%(gap-1) ) + (user+1);
+                    if(comp>=n)
+                    {
+                        if(user<n-1)
+                            comp=n-1;
+                        else
+                            comp=n;
+                    }
+                }
 
-                system ("cls");
+                system("cls");
                 printf("%d\n",comp);
                 if(comp==n)
                 {
@@ -62,15 +85,16 @@ int main()
                     break;
                 }
             }
+            //when user plays beyond rules!!!!
             else
             {
-                system ("cls");
+                system("cls");
                 printf("Dont try to cheat!!!\n");
                 if(comp!=0)
                     printf("%d\n",comp);
-                printf("Enter a number again:");
             }
         }
     }
+
     return 0;
 }
